6045053fad7ac517a59f0a4f18b011a3f28c39f6
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-10-25 08:51:16 +0530

    updated

--- src/Components/Donation/Donation.jsx
+++ src/Components/Donation/Donation.jsx
@@ -1,94 +1,154 @@
-import { useState, useEffect } from "react";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-function Donation({ state, acc }) {
-  const [id, setID] = useState("");
+// import { useState, useEffect } from "react";
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// function Donation({ state, acc }) {
+//   const [id, setID] = useState("");
   
-  const getDonation = async (event) => {
-    try {
-      if (
-        id !== "" 
-      ) {
-        event.preventDefault();
-        const { contract } = state;
-        const accounts = await window.ethereum.request({
-            method: "eth_requestAccounts",
-        });
-        console.log(typeof id);
-        const nameText = await contract.methods.campaigns(id).call();
-        console.log(nameText[1]);
+//   const getDonation = async (event) => {
+//     try {
+//       if (
+//         id !== "" 
+//       ) {
+//         event.preventDefault();
+//         const { contract } = state;
+//         const accounts = await window.ethereum.request({
+//             method: "eth_requestAccounts",
+//         });
+//         console.log(typeof id);
+//         const nameText = await contract.methods.campaigns(id).call();
+//         console.log(nameText[1]);
 
-        if(nameText[1].toLowerCase() === accounts[0]) {
-          await contract.methods
-          .getDonation(
-            id
-          )
-          .send({ from: accounts[0] });
-        }else {
-          toast.error("You are not the Owner of the campaign", {
-            position: "top-right",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "dark",
-          });
-        }
-      }
-    } catch (error) {
-      console.log("Byee");
-    }
-  };
+//         if(nameText[1].toLowerCase() === accounts[0]) {
+//           await contract.methods
+//           .getDonation(
+//             id
+//           )
+//           .send({ from: accounts[0] });
+//         }else {
+//           toast.error("You are not the Owner of the campaign", {
+//             position: "top-right",
+//             autoClose: 5000,
+//             hideProgressBar: false,
+//             closeOnClick: true,
+//             pauseOnHover: true,
+//             draggable: true,
+//             progress: undefined,
+//             theme: "dark",
+//           });
+//         }
+//       }
+//     } catch (error) {
+//       console.log("Byee");
+//     }
+//   };
 
-  const styles = {
-    minHeight: "90vh",
-  };
+//   const styles = {
+//     minHeight: "90vh",
+//   };
+//   return (
+//     <>
+//     <ToastContainer />
+//       <section class="text-gray-400 bg-gray-900 body-font relative">
+//         <form
+//           class="container px-5 py-24 mx-auto"
+//           style={styles}
+//           autocomplete="off"
+//         >
+//           <div class="flex flex-col text-center w-full mb-12">
+//             <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
+//               Compaign Donation 
+//             </h1>
+//           </div>
+//           <div class="lg:w-1/2 md:w-2/3 mx-auto">
+//             <div class="flex flex-wrap -m-2">
+//               <div class="p-2 w-full">
+//                 <div class="relative">
+//                   <label for="name" class="leading-7 text-sm text-gray-400">
+//                     Enter the Complain ID
+//                   </label>
+//                   <input
+//                     type="text"
+//                     autocomplete="off"
+//                     id="name"
+//                     onChange={(e) => setID(Number(e.target.value))}
+//                     class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+//                   />
+//                 </div>
+//               </div>
+//               <div class="p-2 w-full">
+//                 <button
+//                   class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+//                   onClick={getDonation}
+//                 >
+//                   Get Campaign Donation
+//                 </button>
+//               </div>
+//             </div>
+//           </div>
+//         </form>
+//       </section>
+//     </>
+//   );
+// }
+
+// export default Donation;
+import React from 'react';
+
+const Donation = () => {
+  // Static donation data for demonstration
+  const donations = [
+    {
+      campaignTitle: "Startup Idea 1",
+      donatedAmount: 5, // ETH
+      transactionHash: "0x123456789...",
+    },
+    {
+      campaignTitle: "Startup Idea 2",
+      donatedAmount: 10, // ETH
+      transactionHash: "0x987654321...",
+    },
+    {
+      campaignTitle: "Sustainable Packaging Project",
+      donatedAmount: 3, // ETH
+      transactionHash: "0xabcdef123...",
+    },
+    {
+      campaignTitle: "Random Idea 1",
+      donatedAmount: 8, // ETH
+      transactionHash: "0x567812349...",
+    },
+    {
+      campaignTitle: "Random Idea 2",
+      donatedAmount: 15, // ETH
+      transactionHash: "0xfedcba987...",
+    },
+  ];
+
   return (
-    <>
-    <ToastContainer />
-      <section class="text-gray-400 bg-gray-900 body-font relative">
-        <form
-          class="container px-5 py-24 mx-auto"
-          style={styles}
-          autocomplete="off"
-        >
-          <div class="flex flex-col text-center w-full mb-12">
-            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
-              Compaign Donation 
-            </h1>
-          </div>
-          <div class="lg:w-1/2 md:w-2/3 mx-auto">
-            <div class="flex flex-wrap -m-2">
-              <div class="p-2 w-full">
-                <div class="relative">
-                  <label for="name" class="leading-7 text-sm text-gray-400">
-                    Enter the Complain ID
-                  </label>
-                  <input
-                    type="text"
-                    autocomplete="off"
-                    id="name"
-                    onChange={(e) => setID(Number(e.target.value))}
-                    class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  />
-                </div>
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-medium mb-4 text-gray-900">Donation History</h1>
+      <div className="bg-white rounded-lg shadow-md p-4">
+        <ul>
+          {donations.map((donation, index) => (
+            <li key={index} className="mb-2">
+              <div>
+                <span className="text-gray-600 mr-2">Campaign: </span>
+                {donation.campaignTitle}
               </div>
-              <div class="p-2 w-full">
-                <button
-                  class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-                  onClick={getDonation}
-                >
-                  Get Campaign Donation
-                </button>
+              <div>
+                <span className="text-gray-600 mr-2">Donated Amount: </span>
+                {donation.donatedAmount} ETH
               </div>
-            </div>
-          </div>
-        </form>
-      </section>
-    </>
+              <div>
+                <span className="text-gray-600 mr-2">Transaction Hash: </span>
+                {donation.transactionHash}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
-}
+};
 
 export default Donation;
--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,38 +1,26 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 
-function Post({ state, setDetails }) {
-  const [detail, setDetail] = useState([]);
-  const { web3 } = state;
-
-  // For demonstration, we'll add static data for two campaigns
+function Post({ setDetails }) {
   const staticData = [
     {
       id: 1,
-      title: "Static Campaign 1",
-      description: "This is a static campaign description.",
+      title: "Startup Idea 1",
+      description:
+        "This is the description of the first startup idea. It aims to revolutionize the tech industry.",
       image: "image_url_1",
-      targetAmount: web3.utils.toWei("10", "ether"),
+      targetAmount: 1000, // Target amount in ether
       deadline: new Date().getTime() + 7 * 24 * 60 * 60 * 1000, // One week from now
     },
     {
       id: 2,
-      title: "Static Campaign 2",
-      description: "Another static campaign description.",
+      title: "Startup Idea 2",
+      description:
+        "Here's the description of the second startup idea. Join us in making this idea a reality.",
       image: "image_url_2",
-      targetAmount: web3.utils.toWei("5", "ether"),
+      targetAmount: 500, // Target amount in ether
       deadline: new Date().getTime() + 14 * 24 * 60 * 60 * 1000, // Two weeks from now
     },
   ];
-
-  useEffect(() => {
-    const { contract, web3 } = state;
-    const getDetail = async () => {
-      // Instead of fetching data from the contract, we set the static data here
-      setDetail(staticData);
-    };
-    contract && getDetail();
-  }, [state]);
 
   return (
     <div
@@ -45,7 +33,7 @@ function Post({ state, setDetails }) {
         justifyContent: "center",
       }}
     >
-      {detail.map((detail) => (
+      {staticData.map((data) => (
         <div
           className="max-w-2xl overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800"
           style={{
@@ -53,9 +41,9 @@ function Post({ state, setDetails }) {
             marginBottom: "60px",
             maxWidth: "672px",
           }}
-          key={detail.id}
+          key={data.id}
         >
-          <img className="object-cover w-full h-64" src={detail.image} alt="Campaign" />
+          <img className="object-cover w-full h-64" src={data.image} alt="Campaign" />
           <div className="p-6">
             <div>
               <a
@@ -64,16 +52,16 @@ function Post({ state, setDetails }) {
                 tabIndex="0"
                 role="link"
               >
-                {detail.title}
+                {data.title}
               </a>
               <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
-                {detail.description}
+                {data.description}
               </p>
               <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
-                Target Amount: {web3.utils.fromWei(detail.targetAmount.toString(), "ether")} ETH
+                Target Amount: {data.targetAmount} ETH
               </p>
               <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
-                Deadline: {new Date(detail.deadline).toString()}
+                Deadline: {new Date(data.deadline).toString()}
               </p>
             </div>
             <div className="mt-4">
@@ -81,4 +69,18 @@ function Post({ state, setDetails }) {
                 <button
                   className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
                   onClick={() => {
-                    setDet
+                    setDetails(data);
+                  }}
+                >
+                  View
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Post;
